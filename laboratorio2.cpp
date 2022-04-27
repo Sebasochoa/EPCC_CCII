772d8d7d649f86e77fd236240df99af40920c50e
Sebasochoa <sochoao@unsa.edu.pe>
2022-04-27 07:38:51 -0500

    23

---
+++ laboratorio2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <conio.h>
+#include <stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    int n,promedio;
+    cout <<"Numero de alumnos: ";
+    cin >>n;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Estudiante N "<<i+1<<endl;
+        float sum = 0;
+        for (int j = 0; j< 3; j++)
+        {
+            cout<<"Nota del estudiante "<<j + 1<<": ";
+            cin >> promedio;
+            sum += promedio;
+        }
+        cout<<"Promedio de nota del estudiante N "<<i+1<<" es "<<sum / 3<<endl;
+    }
+    return 0;
+}
