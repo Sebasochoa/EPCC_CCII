3e17b2d334526fb143fc5cc4737abd9d84e061df
Sebasochoa <sochoao@unsa.edu.pe>
2022-06-21 16:33:18 -0500

    Teoria

--- Teoria/Segunda Unidad/Semana11/contador.cpp
+++ Teoria/Segunda Unidad/Semana11/contador.cpp
@@ -1,25 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class contador
 {
 private:
-    int cont=0;
+    int cont;
+    static auto getcontador() -> size_t& {
+        static size_t contador = 0;
+        return contador;
+    }
+
 public:
-    contador(int);
-    ~contador();
-    int valor();
+    contador(int contador = 0)
+        : cont{ contador }
+    {
+        getcontador()++;
+    }
+
+    ~contador()
+    {
+        getcontador()--;
+    }
+
+    contador &operator++()
+    {
+        ++cont;
+        return *this;
+    }
+    contador operator++(int)
+    {
+        contador temp{*this};
+        ++(*this);
+        return temp;
+    }
+
+    int valor()
+    {
+        return cont;
+    }
+    static auto getCount() -> std::size_t {
+        return getcontador();
+    }
 };
-
-contador::contador(int cont)
+int main()
 {
-    this->cont=cont;
-}
-
-contador::~contador()
-{
-}
-
-int contador::valor()
-{
-    return cont;
+    contador c1(1);
+    contador c2(2);
+    cout<<c1.getCount();
 }
